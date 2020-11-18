cb6905fe9eea89df23c6007639ef14013088c425
Christian Bain <christianbain@me.com>
2020-11-18 09:26:45 -0700

    Initial Commit

---
+++ LizB/LizBApp.swift
@@ -0,0 +1,17 @@
+//
+//  LizBApp.swift
+//  LizB
+//
+//  Created by Christian Bain on 11/18/20.
+//
+
+import SwiftUI
+
+@main
+struct LizBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
